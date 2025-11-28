971068f12227f747afc272d30f8975cd062cc052
Rajeshvissa <126651798+Rajeshvissa@users.noreply.github.com>
2025-11-28 20:13:40 +0530

    Delete backend directory

--- backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-function generateToken(id, role, name) {
-  return jwt.sign({ id, role, name }, process.env.JWT_SECRET, {
-    expiresIn: "7d"
-  });
-}
-
-module.exports = generateToken;
